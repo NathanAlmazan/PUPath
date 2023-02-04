e15506fd9b8b0968f38ea331d922dfb7e40dbcc5
= <=>
2023-02-04 20:11:51 +0800

    update implementing class

--- src/com/nathanael/Main.java
+++ src/com/nathanael/Main.java
@@ -6,8 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        GraphGenerator generator = new GraphGenerator();
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Good Day! Where do you want to go?");
         String inputDestination = scanner.nextLine();
@@ -17,8 +15,12 @@ public class Main {
 
         scanner.close();
 
-        Integer destination = generator.analyzeChat(inputDestination);
-        Integer location = generator.analyzeChat(inputLocation);
+        // cannot be changed
+
+        GraphGenerator generator = new GraphGenerator();
+
+        Integer destination = generator.analyzeChat(inputDestination); // put the destination chat response
+        Integer location = generator.analyzeChat(inputLocation); // put the current location chat response
 
         System.out.println("Finding path from " + GraphGenerator.destinations.get(location) + " to " + GraphGenerator.destinations.get(destination) + "...");
 
@@ -27,9 +29,14 @@ public class Main {
 
         Collections.reverse(paths);
 
-        System.out.println("Follow these steps to arrive at your destination: ");
+        List<String> instructions = generator.generateInstructions(paths);
 
-        for (String step : generator.generateInstructions(paths))
+        // variable instruction contains the steps
+
+
+        // change on how you will display the data
+        System.out.println("Follow these steps to arrive at your destination: ");
+        for (String step : instructions)
             System.out.println(step);
     }
 }
